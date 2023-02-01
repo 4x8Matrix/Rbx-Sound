d27c711979e0d1543ecd4fcfe236195eedb206e2
AsyncMatrix <Catbob614@gmail.com>
2023-02-01 08:44:56 +0000

    ✨ Updated documentation to support 1.0.3 version

--- Source/init.lua
+++ Source/init.lua
@@ -17,6 +17,8 @@
 			- Appended a ChangeLog & Updated header note
 			- Removing LuaU 'strictness' flag
 			- Updated package name from 'rbxsound' -> 'RbxSound'
+		1.0.3:
+			- Addition of a README & Updating the Wally Package
 ]]--
 
 -- // SERVICES
